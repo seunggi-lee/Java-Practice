0eb60fd5156e3adc7868d7d6fb72a41609ebb017
이승기 <@DESKTOP-CIRICQI>
2022-02-15 20:34:05 +0900

    클래스 관련해서 추가적인 공부 --> 여러 개의 클래스를 이용하는 방식과 접근제어자

--- ClassPractice/src/classpart/Student.java
+++ ClassPractice/src/classpart/Student.java
@@ -7,6 +7,12 @@ public class Student {
 		int grade;
 		String address;
 		
+//		public Student() {	} // 생성하지 않으면 자동으로 생긴다.(매개변수가 없는 생성자 자동 생성)
+
+//		public Student(int id, String name) {
+//			studentID = id;
+//			studentName = name;
+//		}
 		public void showStudentInfor() {
 			System.out.println(studentName + ", " + address);
 		}
@@ -17,4 +23,25 @@ public class Student {
 //			
 //			studentLee.showStudentInfor();
 //		}
+		
+		public String getStudentName() {
+			return studentName;
+		}
+		public void setStudentName(String name) {
+			studentName = name;
+		}
+		public static void main(String[] args) {
+			Student studentLee = new Student();
+			studentLee.studentName= "이순신";
+			studentLee.studentID = 100;
+			studentLee.address = "서울시 영등포구 여의도동";
+			
+			Student studentkim = new Student();
+			studentkim.studentName= "김유신";
+			studentkim.studentID = 101;
+			studentkim.address = "미국 산호세";
+			
+			studentLee.showStudentInfor();
+			studentkim.showStudentInfor();
+		}
 }
