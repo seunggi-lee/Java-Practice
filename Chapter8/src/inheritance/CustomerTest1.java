7e586dca2a568e2736af7438f42e35b97c24259c
이승기 <@DESKTOP-CIRICQI>
2022-02-16 14:30:06 +0900

    상속과 다형성

---
+++ Chapter8/src/inheritance/CustomerTest1.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+public class CustomerTest1 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Customer customerLee = new Customer(); customerLee.setCustomerID(10100);
+		 * customerLee.setCustomerName("Lee");
+		 */
+		
+		VipCustomer customerKim = new VipCustomer();
+		customerKim.setCustomerID(10101);
+		customerKim.setCustomerName("Kim");
+		
+		customerKim.bonusPoint = 1000;
+		
+		
+//		System.out.println(customerLee.showCustomerInfo());
+		System.out.println(customerKim.showCustomerInfo());
+	}
+
+}
