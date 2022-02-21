4dcaf309fcd0cc27d17ca1e474b3a12c1df08665
이승기 <won9704@naver.com>
2022-02-21 15:07:08 +0900

    Test2

--- WindowBuilderTest/src/windowbuilder/WindowBuilderTest.java
+++ WindowBuilderTest/src/windowbuilder/WindowBuilderTest.java
@@ -3,6 +3,21 @@ package windowbuilder;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Color;
+import java.awt.Insets;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class WindowBuilderTest {
 
@@ -36,8 +51,37 @@ public class WindowBuilderTest {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 685, 557);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.ORANGE);
+		panel.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.RED);
+		panel.add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.GREEN);
+		panel.add(panel_3);
+		
+		JPanel panel2 = new JPanel();
+		frame.getContentPane().add(panel2);
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS));
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Color.PINK);
+		panel2.add(panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.BLUE);
+		panel2.add(panel_5);
 	}
 
 }
